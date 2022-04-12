338d884b0a762f691d7e7cd350af4a546a0bb8a8
dbagley1 <59030213+dbagley1@users.noreply.github.com>
2022-04-11 23:00:24 -0400

    Restoring original functionality to test

--- server.ts
+++ server.ts
@@ -1,2 +1,15 @@
 import { serve } from "https://deno.land/std@0.120.0/http/server.ts";
-serve((_req) => new Response("Hello, world"));
+serve((req) => {
+  const output = [];
+
+  let url = new URL(req.url);
+  output.push(url);
+
+  let path = url.pathname;
+  output.push(path);
+
+  let word = path.match(/\w+/)[0]
+  output.push(word);
+
+  return new Response(output.join('\n'));
+}
